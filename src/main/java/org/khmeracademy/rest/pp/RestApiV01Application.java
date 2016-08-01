bb4672b5a2b7e99cce3bd6c80c26c7247161a6ae
youraleangseng <youra.leangseng@gmail.com>
2016-08-01 23:49:49 +0700

    v1

---
+++ src/main/java/org/khmeracademy/rest/pp/RestApiV01Application.java
@@ -0,0 +1,12 @@
+package org.khmeracademy.rest.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiV01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiV01Application.class, args);
+	}
+}
